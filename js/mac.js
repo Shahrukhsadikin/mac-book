cac8c33795316d996b961a4d16c2d01c61d6c4bb
Shahrukhsadikin <shahrukh826966@gmail.com>
2021-08-21 22:29:43 +0600

    3rd commit

--- js/mac.js
+++ js/mac.js
@@ -2,48 +2,56 @@ document.getElementById('cost-memory0').addEventListener('click',  function(){
     const oldPrice = document.getElementById('Extra-Memory');
     const updatePrice = oldPrice.innerText;
     oldPrice.innerText = 0; 
+    updateTotal()
 })
 document.getElementById('cost-memory16').addEventListener('click',  function(){
     const oldPrice = document.getElementById('Extra-Memory');
     const updatePrice = oldPrice.innerText;
     oldPrice.innerText = 180; 
+    updateTotal()
 })
 // storage
 document.getElementById('cost-storage256').addEventListener('click',  function(){
     const oldPrice = document.getElementById('Extra-Storage');
     const updatePrice = oldPrice.innerText;
-    oldPrice.innerText = 0; 
+    oldPrice.innerText = 0;
+    updateTotal() 
 })
 document.getElementById('cost-storage512').addEventListener('click',  function(){
     const oldPrice = document.getElementById('Extra-Storage');
     const updatePrice = oldPrice.innerText;
     oldPrice.innerText = 100; 
+    updateTotal()
 })
 document.getElementById('cost-storage1TB').addEventListener('click',  function(){
     const oldPrice = document.getElementById('Extra-Storage');
     const updatePrice = oldPrice.innerText;
     oldPrice.innerText = 180; 
+    updateTotal()
 })
 
 // delivary charge
 document.getElementById('free-delivery').addEventListener('click',  function(){
-    const oldPrice = document.getElementById('total-price');
+    const oldPrice = document.getElementById('Delivery-Charge');
     const updatePrice = oldPrice.innerText;
     oldPrice.innerText = 0; 
+    updateTotal()
 })
 document.getElementById('charge-delivery').addEventListener('click',  function(){
     const oldPrice = document.getElementById('Delivery-Charge');
     const updatePrice = oldPrice.innerText;
-    oldPrice.innerText = 20; 
+    oldPrice.innerText = 20;
+    updateTotal() 
 })
 
 function updateTotal(){
-    const totalPrice=document.getElementById('total-price');
+    
    
     const bestPrice=parseInt(document.getElementById('Best-Price').innerText);
     const memoryPrice =parseInt(document.getElementById('Extra-Memory').innerText);
     const storagePrice =parseInt(document.getElementById('Extra-Storage').innerText);
     const deliveryPrice =parseInt(document.getElementById('Delivery-Charge').innerText);
+    const totalPrice=document.getElementById('total-price');
     
     totalPrice.innerText=bestPrice+memoryPrice+storagePrice+deliveryPrice;
 
